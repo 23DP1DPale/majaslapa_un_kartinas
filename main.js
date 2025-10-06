14ddf985bc2b5e5c713e6eff8811b6cd1767c6cf
Daniels Palejs <166118206+23DP1DPale@users.noreply.github.com>
2025-10-06 17:45:10 +0000

    Pievienots burger menu

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-const dark_mode = document.getElementById("dark-mode");
-const navigation = document.getElementById("nav-section")
-const background = document.getElementById("main-section");
-const cards = document.querySelectorAll(".card");
-
-dark_mode.addEventListener("click", () => {
-    navigation.classList.toggle("dark-mode");
-    background.classList.toggle("dark-mode");
-    cards.forEach((card) => {
-        card.classList.toggle("dark-mode-card");
-    })
+const hamMenu = document.getElementById("ham-menu");
+const offscreenNavigation = document.getElementById("navigation");
+hamMenu.addEventListener('click', () => {
+    hamMenu.classList.toggle('active');
+    offscreenNavigation.classList.toggle('active');
 })
