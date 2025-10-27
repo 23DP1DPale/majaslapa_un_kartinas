7019535056f1d08f4a63e90765056e777ef44fc9
Daniels Palejs <166118206+23DP1DPale@users.noreply.github.com>
2025-10-27 08:47:15 +0000

    Pabeigta dinamiska satura ģenerēšana, un uztaisīta meklēšanas pirmā versija.

--- card.js
+++ card.js
@@ -1,12 +1,12 @@
 const dark_mode = document.getElementById("dark-mode");
-const navigation = document.getElementById("nav-section")
-const background = document.getElementById("main-section");
+const body = document.getElementById("bdy");
 const cards = document.querySelectorAll(".card");
-const cardContainer = document.getElementById("main-section")
+const cardContainer = document.getElementById("card-section");
+const searchContent = document.getElementById("search-content");
+const searchBtn = document.getElementById("search-btn");
 
 dark_mode.addEventListener("click", () => {
-    navigation.classList.toggle("dark-mode");
-    background.classList.toggle("dark-mode");
+    body.classList.toggle("dark-mode");
     cards.forEach((card) => {
         card.classList.toggle("dark-mode-card");
     })
@@ -16,22 +16,33 @@ const products = [
     {name: "Stanley Termokrūze", image: "images/stanley.jpg", desc: "Nerūsējošā tērauda termokrūze ar vakuuma izolāciju, kas uztur dzērienu karstu līdz 7h."},
     {name: "Xiaomi Mi Smart Band", image: "images/watch.jpg", desc: "Gudrais fitnesa aproce ar treniņu režīmiem, sirdsdarbības un miega monitoringu."},
     {name: "Kafijas automāts", image: "images/kafija.jpg", desc: "Kompakts un ērts kafijas aparāts ar kapsulām gardai kafijai mājās."}
-]
+];
 
-products.forEach(product => {
-    const card = document.createElement("div");
-    card.classList.add("card");
-
-    card.innerHTML = `
-        <h1>${product.name}</h1>
-            <img alt="image" src=${product.image}">
-            <p>${product.desc}</p>
-            <div class="button">
-                <div>
-                    <div></div>
-                </div>
-            </div>
-    `;
-
-    cardContainer.append(card);
+searchBtn.addEventListener("click", () => {
+    showCards();
 });
+
+
+function showCards() {
+    cardContainer.innerHTML = "";
+    products.forEach(product => {
+        let card = null;
+        if (product.name.search(searchContent.value) != -1) {
+            card = document.createElement("div");
+            card.classList.add("card");
+            card.innerHTML = `
+                <h1>${product.name}</h1>
+                    <img alt="image" src=${product.image}>
+                    <p>${product.desc}</p>
+                    <div class="button">
+                        <div>
+                            <div></div>
+                        </div>
+                    </div>
+            `;
+            cardContainer.append(card);
+        }
+    });
+};
+
+window.onload = showCards();
