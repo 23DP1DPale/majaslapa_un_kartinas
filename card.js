83fdcc4b9098dd119ed1d1ec5957ac741612accc
Daniels Palejs <166118206+23DP1DPale@users.noreply.github.com>
2025-11-05 12:52:10 +0000

    Papildināta sortēšana

--- card.js
+++ card.js
@@ -13,11 +13,11 @@ dark_mode.addEventListener("click", () => {
 })
 
 const products = [
-    {name: "Stanley Termokrūze", image: "images/stanley.jpg", desc: "Nerūsējošā tērauda termokrūze ar vakuuma izolāciju, kas uztur dzērienu karstu līdz 7h."},
-    {name: "Xiaomi Mi Smart Band", image: "images/watch.jpg", desc: "Gudrais fitnesa aproce ar treniņu režīmiem, sirdsdarbības un miega monitoringu."},
-    {name: "Kafijas automāts", image: "images/kafija.jpg", desc: "Kompakts un ērts kafijas aparāts ar kapsulām gardai kafijai mājās."}
+    {prod: "Atanley Termokrūze", image: "images/stanley.jpg", desc: "Nerūsējošā tērauda termokrūze ar vakuuma izolāciju, kas uztur dzērienu karstu līdz 7h."},
+    {prod: "Ciaomi Mi Smart Band", image: "images/watch.jpg", desc: "Gudrais fitnesa aproce ar treniņu režīmiem, sirdsdarbības un miega monitoringu."},
+    {prod: "Bafijas automāts", image: "images/kafija.jpg", desc: "Kompakts un ērts kafijas aparāts ar kapsulām gardai kafijai mājās."}
 ];
-
+console.log(products)
 searchBtn.addEventListener("click", () => {
     showCards();
 });
@@ -26,25 +26,27 @@ const sortAZ = document.getElementById("sort-az");
 const sortZA = document.getElementById("sort-za");
 
 sortZA.addEventListener("click", () => {
-    showCards(az);
+    showCards("az");
 });
 
 sortAZ.addEventListener("click", () => {
-    showCards(za);
+    showCards("za");
 });
 
 function showCards(sort) {
     cardContainer.innerHTML = "";
-    let sortedProducts = Object.fromEntries(
-        Object.entries(products).sort();
-    );
+    if (sort == "az") {
+        products.sort((a, b) => a.prod.localeCompare(b.prod));
+    } else if (sort = "za") {
+        products.sort((a, b) => b.prod.localeCompare(a.prod));
+    }
     products.forEach(product => {
         let card = null;
-        if (product.name.toLowerCase().search(searchContent.value.toLowerCase()) != -1) {
+        if (product.prod.toLowerCase().search(searchContent.value.toLowerCase()) != -1) {
             card = document.createElement("div");
             card.classList.add("card");
             card.innerHTML = `
-                <h1>${product.name}</h1>
+                <h1>${product.prod}</h1>
                     <img alt="image" src=${product.image}>
                     <p>${product.desc}</p>
                     <div class="button">
